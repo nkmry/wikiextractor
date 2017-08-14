9913dfb356ced13ee81ca5822bd67737dc598714
nkmry <nkmry333@gmail.com>
2017-08-14 10:02:56 +0900

    add normalize_neologd() to normalize sentences for MeCab

--- jawiki_script.py
+++ jawiki_script.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+import unicodedata
 import re, sys, os, glob, argparse
 from pathlib import Path
 from tqdm import tqdm
@@ -8,6 +10,7 @@ def delete_html_tag_with_contents(tag: str, src: str):
     return delete_regexps(
                 "<{tag}[^>]*>[\s\S]*?<\s*/{tag}\s*>".format(tag=tag), src)
 
+
 def delete_regexps(regexp: str, src: str):
     I = [(i.start(), i.end()) for i in re.finditer(regexp, src)]
     if len(I) == 0:
@@ -17,6 +20,61 @@ def delete_regexps(regexp: str, src: str):
         res += src[I[i][1]:I[i+1][0]]
     res += src[I[-1][1]:]
     return res
+
+
+def unicode_normalize(cls, s):
+    pt = re.compile('([{}]+)'.format(cls))
+
+    def norm(c):
+        return unicodedata.normalize('NFKC', c) if pt.match(c) else c
+
+    s = ''.join(norm(x) for x in re.split(pt, s))
+    s = re.sub('－', '-', s)
+    return s
+
+
+def remove_extra_spaces(s):
+    s = re.sub('[ 　]+', ' ', s)
+    blocks = ''.join(('\u4E00-\u9FFF',  # CJK UNIFIED IDEOGRAPHS
+                      '\u3040-\u309F',  # HIRAGANA
+                      '\u30A0-\u30FF',  # KATAKANA
+                      '\u3000-\u303F',  # CJK SYMBOLS AND PUNCTUATION
+                      '\uFF00-\uFFEF'   # HALFWIDTH AND FULLWIDTH FORMS
+                      ))
+    basic_latin = '\u0000-\u007F'
+
+    def remove_space_between(cls1, cls2, s):
+        p = re.compile('([{}]) ([{}])'.format(cls1, cls2))
+        while p.search(s):
+            s = p.sub(r'\1\2', s)
+        return s
+
+    s = remove_space_between(blocks, blocks, s)
+    s = remove_space_between(blocks, basic_latin, s)
+    s = remove_space_between(basic_latin, blocks, s)
+    return s
+
+
+def normalize_neologd(s):
+    s = s.strip()
+    s = unicode_normalize('０-９Ａ-Ｚａ-ｚ｡-ﾟ', s)
+
+    def maketrans(f, t):
+        return {ord(x): ord(y) for x, y in zip(f, t)}
+
+    s = re.sub('[˗֊‐‑‒–⁃⁻₋−]+', '-', s)  # normalize hyphens
+    s = re.sub('[﹣－ｰ—―─━ー]+', 'ー', s)  # normalize choonpus
+    s = re.sub('[~∼∾〜〰～]', '', s)  # remove tildes
+    s = s.translate(
+        maketrans('!"#$%&\'()*+,-./:;<=>?@[¥]^_`{|}~｡､･｢｣',
+              '！”＃＄％＆’（）＊＋，－．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」'))
+
+    s = remove_extra_spaces(s)
+    s = unicode_normalize('！”＃＄％＆’（）＊＋，－．／：；＜＞？＠［￥］＾＿｀｛｜｝〜', s)  # keep ＝,・,「,」
+    s = re.sub('[’]', '\'', s)
+    s = re.sub('[”]', '"', s)
+    return s
+
 
 def postprocess(filename: str):
     file_path = Path(filename)
@@ -37,8 +95,10 @@ def postprocess(filename: str):
     for _ in range(5): # to handle nested brackets
         s = delete_regexps("[(（][^()（）]*[)）]", s)
         s = delete_regexps("(|)|（|）", s)
+    s = normalize_neologd(s)
     with (file_path.parent / ('modified_' + file_path.name)).open('w') as f:
         f.write(s)
+
 
 def main():
     parser = argparse.ArgumentParser()
